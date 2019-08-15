b3fa03e8bb30e335c0ccb8617235280772cdf4d7
bfelda <bfelda@gmail.com>
2019-08-14 20:15:56 -0500

    added passthrough subscribe folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("subscribe");
     eleventyConfig.addPassthroughCopy("css");
     
     return {
